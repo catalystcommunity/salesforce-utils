9dc0675b64bd9bb0c9d71deaf0c30eeea1c79d37
benjyiw <ben@wese.io>
2022-09-12 16:13:09 -0600

    fix: allow configuring a custom fasthttp client

--- pkg/common.go
+++ pkg/common.go
@@ -2,14 +2,15 @@ package pkg
 
 import (
 	"fmt"
+
 	"github.com/valyala/fasthttp"
 )
 
 // sendRequest sends a configured request, returning the body, status code, and error
-func (s SalesforceUtils) sendRequest(req *fasthttp.Request) ([]byte, int, error) {
+func (s *SalesforceUtils) sendRequest(req *fasthttp.Request) ([]byte, int, error) {
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", s.Credentials.AccessToken))
 	res := fasthttp.AcquireResponse()
 	defer fasthttp.ReleaseResponse(res)
-	err := fasthttp.Do(req, res)
+	err := s.FastHTTPClient.Do(req, res)
 	return res.Body(), res.StatusCode(), err
 }
